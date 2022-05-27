b75d9fb4c46a9ab301b9a2e8627fa6a9ea4b3ce6
Ronald <rjskinnerc@unal.edu.co>
2022-05-27 17:18:22 -0500

    Add nested routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
     </div>
   );
 }
